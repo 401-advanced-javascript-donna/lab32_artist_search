4159e2dc390a70ad0393dc951a699bb73e809805
sanvean74 <dlambertpdx@gmail.com>
2019-11-07 17:34:56 -0800

    hooks refactor on lyrics display

--- src/container/LyricsDisplay.js
+++ src/container/LyricsDisplay.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { fetchLyrics } from '../services/api-call';
 import Lyrics from '../components/Lyrics';
 
-export default class LyricsDisplay extends Component {
-  static propTypes = {
-    match: PropTypes.shape({
-      params: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        track: PropTypes.string.isRequired
-      }).isRequired
-    }).isRequired
-  }
 
-  state = {
-    lyrics: ''
-  }
+const LyricsDisplay = ({ match }) => {
+  const [lyrics, setLyrics] = useState('');
 
-  componentDidMount() {
-    fetchLyrics(this.props.match.params.name, this.props.match.params.track)
+
+  useEffect(() => {
+    fetchLyrics(match.params.name, match.params.track)
       .then(res => {  
-        this.setState({ lyrics: res });
+        setLyrics(res);
       });
-  }
+  });
+  
+  return (
+    <Lyrics lyrics={lyrics}
+      name={match.params.name}
+      title={match.params.track} />
+  );
+};
 
-  render() {
-    return (
-      <Lyrics lyrics={this.state.lyrics}
-        name={this.props.match.params.name}
-        title={this.props.match.params.track} />
-    );
-  }
-}
+LyricsDisplay.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      track: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
+};
+
+export default LyricsDisplay;
